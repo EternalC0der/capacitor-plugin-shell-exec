2b2c5227e89d0d0ac5b4caaff0254d878547b406
EternalC0der <s.eternalcoder@gmail.com>
2024-08-01 12:39:02 +0330

    Bug fix

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,3 @@
-declare module '@capacitor/core' {
-  interface PluginRegistry {
-    ShellExec: ShellExecPlugin;
-  }
-}
-
 export interface ShellExecPlugin {
   execute(options: { command: string }): Promise<{ output: string; exitCode: number }>;
 }
