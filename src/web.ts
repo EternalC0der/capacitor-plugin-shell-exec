b4474a76e0dd596d5ad11b3f11f07dc3ca0ba24f
EternalC0der <s.eternalcoder@gmail.com>
2024-08-01 12:33:45 +0330

    Minor improvements

--- src/web.ts
+++ src/web.ts
@@ -10,6 +10,9 @@ export class ShellExecWeb extends WebPlugin implements ShellExecPlugin {
   }
 
   async execute(options: { command: string }): Promise<{ output: string; exitCode: number }> {
+    // To suppress the unused parameter warning
+    void options;
+
     console.log('This function is not available in the web');
     return { output: '', exitCode: -1 };
   }
